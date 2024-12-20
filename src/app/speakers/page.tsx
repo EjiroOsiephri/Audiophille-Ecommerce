a09ec533dd4c05323343468aa5dc9201a8a5ac76
ejiro osiephri <osiephriejiro765@gmail.com>
2024-12-20 21:20:24 +0100

    feats/add added speakers components

--- src/app/speakers/page.tsx
+++ src/app/speakers/page.tsx
@@ -1,9 +1,17 @@
 import Navbar from "@/components/Navbar";
+import SpeakersComponent from "@/components/Speakers/SpeakersComponent";
+import CategorySection from "@/components/CategorySection";
+import AboutUs from "@/components/AboutUs";
+import Footer from "@/components/Footer";
 
 export default function Speakers() {
   return (
     <div>
       <Navbar />
+      <SpeakersComponent />
+      <CategorySection />
+      <AboutUs />
+      <Footer />
     </div>
   );
 }
--- src/components/Speakers/SpeakersComponent.tsx
+++ src/components/Speakers/SpeakersComponent.tsx
@@ -1,25 +1,22 @@
 import Image from "next/image";
 import Link from "next/link";
-import imageone from "../../../public/image-removebg-preview(47).png";
-import imagetwo from "../../../public/image-removebg-preview(41) (1).png";
-import imagethree from "../../../public/image-removebg-preview(48).png";
+import imageone from "../../../public/image-removebg-preview(3).png";
+import imagetwo from "../../../public/image-removebg-preview(49).png";
 
-const HeadphonesComponent = () => {
+const SpeakersComponent = () => {
   return (
     <div className="bg-gray-50">
-      {/* Header Section */}
       <header className="bg-black tracking-[0.15em] text-white py-14 mb-10 text-center">
-        <h1 className="text-3xl font-bold uppercase">Headphones</h1>
+        <h1 className="text-3xl font-bold uppercase">Speakers</h1>
       </header>
 
       {/* Product Section */}
       <section className="px-6 md:px-20 py-16 space-y-24">
-        {/* XX99 Mark II Headphones */}
         <div className="flex flex-col md:flex-row items-center gap-12">
           <div className="relative w-full h-[350px] md:w-1/2 bg-[#F1F1F1] rounded-lg flex items-center justify-center">
             <Image
               src={imageone}
-              alt="XX99 Mark II Headphones"
+              alt="ZX9-SPEAKER"
               width={500}
               height={350}
               className="rounded-lg"
@@ -31,15 +28,16 @@ const HeadphonesComponent = () => {
               New Product
             </p>
             <h2 className="text-2xl text-[#000000] font-bold uppercase mt-4">
-              XX99 Mark II Headphones
+              ZX9 SPEAKER
             </h2>
             <p className="text-gray-600 mt-4">
-              The XX99 Mark II headphones redefine the pinnacle of pristine
-              audio. These headphones combine unparalleled style, comfort, and
-              sound quality.
+              Upgrade your sound system with the all new ZX9 active speaker.
+              It’s a bookshelf speaker system that offers truly wireless
+              connectivity -- creating new possibilities for more pleasing and
+              practical audio setups.
             </p>
             <Link
-              href="/products/xx99-mark-ii"
+              href="/products/ZX9"
               className="inline-block mt-6 py-2 px-6 bg-[#D87D4A] text-white uppercase text-sm font-bold rounded-lg hover:bg-[#f09c6a]"
             >
               See Product
@@ -47,12 +45,11 @@ const HeadphonesComponent = () => {
           </div>
         </div>
 
-        {/* XX99 Mark I Headphones */}
         <div className="flex flex-col md:flex-row-reverse items-center gap-12">
           <div className="relative w-full h-[350px] md:w-1/2 bg-[#F1F1F1] rounded-lg flex items-center justify-center">
             <Image
               src={imagetwo}
-              alt="XX99 Mark I Headphones"
+              alt="ZX7 SPEAKER"
               width={500}
               height={350}
               className="rounded-lg"
@@ -61,44 +58,16 @@ const HeadphonesComponent = () => {
           </div>
           <div className="md:w-1/2 w-full max-w-lg flex flex-col md:text-left text-center md:items-start items-center md:pl-6">
             <h2 className="text-2xl text-[#000000] font-bold uppercase mt-4">
-              XX99 Mark I Headphones
+              ZX7 SPEAKER
             </h2>
             <p className="text-gray-600 mt-4">
-              As the gold standard for headphones, the classic XX99 Mark I
-              offers detailed and accurate audio reproduction for audiophiles
-              and music lovers alike.
+              Stream high quality sound wirelessly with minimal loss. The ZX7
+              bookshelf speaker uses high-end audiophile components that
+              represents the top of the line powered speakers for home or studio
+              use.
             </p>
             <Link
-              href="/products/xx99-mark-i"
-              className="inline-block mt-6 py-2 px-6 bg-[#D87D4A] text-white uppercase text-sm font-bold rounded-lg hover:bg-[#f09c6a]"
-            >
-              See Product
-            </Link>
-          </div>
-        </div>
-
-        {/* XX59 Headphones */}
-        <div className="flex flex-col md:flex-row items-center gap-12">
-          <div className="relative w-full h-[350px] md:w-1/2 bg-[#F1F1F1] rounded-lg flex items-center justify-center">
-            <Image
-              src={imagethree}
-              alt="XX59 Headphones"
-              width={500}
-              height={350}
-              className="rounded-lg"
-              style={{ objectFit: "contain", width: "auto", height: "auto" }}
-            />
-          </div>
-          <div className="md:w-1/2 w-full max-w-lg flex flex-col md:text-left text-center md:items-start items-center md:pl-6">
-            <h2 className="text-2xl text-[#000000] font-bold uppercase mt-4">
-              XX59 Headphones
-            </h2>
-            <p className="text-gray-600 mt-4">
-              Enjoy your audio anywhere and customize it to your specific needs
-              with the XX59 headphones. Perfect for all situations on the go.
-            </p>
-            <Link
-              href="/products/xx59"
+              href="/products/ZX7"
               className="inline-block mt-6 py-2 px-6 bg-[#D87D4A] text-white uppercase text-sm font-bold rounded-lg hover:bg-[#f09c6a]"
             >
               See Product
@@ -110,4 +79,4 @@ const HeadphonesComponent = () => {
   );
 };
 
-export default HeadphonesComponent;
+export default SpeakersComponent;
