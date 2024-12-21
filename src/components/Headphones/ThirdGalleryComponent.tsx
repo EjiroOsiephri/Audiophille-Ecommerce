52f95eaa395edbf1ae93b8f38f4d06d95609e5df
ejiro osiephri <osiephriejiro765@gmail.com>
2024-12-21 11:11:09 +0100

    feats/add done with headsets routes and all

--- src/components/Headphones/ThirdGalleryComponent.tsx
+++ src/components/Headphones/ThirdGalleryComponent.tsx
@@ -53,23 +53,24 @@ const ThirdGalleryComponent = () => {
         </h2>
 
         {/* Products */}
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 mb-16">
           {[product1, product2, product3].map((product, index) => (
             <div
               key={index}
-              className="flex flex-col items-center rounded-lg p-6 "
+              className="flex flex-col items-center rounded-lg p-8"
+              style={{ height: "360px", width: "100%" }}
             >
-              <div className="relative p-6 bg-gray-100 rounded-lg w-40 h-40 flex items-center justify-center">
+              <div className="relative p-12 bg-gray-200 rounded-lg w-60 h-60 flex items-center justify-center">
                 <Image
                   src={product}
                   alt={`Product ${index + 1}`}
                   layout="intrinsic"
-                  width={130}
-                  height={130}
+                  width={200}
+                  height={200}
                   className="rounded-lg"
                 />
               </div>
-              <h3 className="mt-4 text-[#000000] text-lg font-semibold uppercase text-center">
+              <h3 className="mt-6 text-[#000000] text-xl font-semibold uppercase text-center">
                 {index === 0
                   ? "XX99 MARK I"
                   : index === 1
@@ -77,8 +78,14 @@ const ThirdGalleryComponent = () => {
                   : "ZX9 SPEAKER"}
               </h3>
               <Link
-                href="#"
-                className="mt-4 px-4 py-2 bg-[#D87D4A] text-white text-sm font-bold uppercase rounded-md hover:bg-[#f09c6a]"
+                href={
+                  index === 0
+                    ? "/products/thirdheadset"
+                    : index === 1
+                    ? "/products/secondheadset"
+                    : "/products/firstspeaker"
+                }
+                className="mt-4 px-6 py-3 bg-[#D87D4A] text-white text-sm font-bold uppercase rounded-md hover:bg-[#f09c6a]"
               >
                 See Product
               </Link>
