a4408c392534c4e1754d6ad9bb811c024882d0bb
ejiro osiephri <osiephriejiro765@gmail.com>
2024-12-21 03:32:16 +0100

    feats/add done with another section of the products page

--- src/app/products/firstheadset/page.tsx
+++ src/app/products/firstheadset/page.tsx
@@ -1,12 +1,14 @@
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import FirstHeadPhoneComponent from "@/components/Headphones/FirstHeadPhoneComponent";
+import GalleryComponent from "@/components/GalleryComponents";
 
 export default function Firstheadset() {
   return (
     <div>
       <Navbar />
       <FirstHeadPhoneComponent />
+      <GalleryComponent />
       <Footer />
     </div>
   );
